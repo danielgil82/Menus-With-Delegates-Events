448ff5fac2c4d16a8421497b869bd5afd85fa661
Daniel Gil <danielgil@my.hit.ac.il>
2021-05-25 10:28:05 +0300

    Refractored

--- Menus-With-Delegates-Events/Menus.Delegates/ButtonCountSpaces.cs
+++ Menus-With-Delegates-Events/Menus.Delegates/ButtonCountSpaces.cs
@@ -6,41 +6,44 @@ namespace Menus.Delegates
 {
     public class ButtonCountSpaces : MenuItem
     {
-        private const string k_Title = "CountSpaces";
-
-        public ButtonCountSpaces() : base(k_Title)
+        public class temp
         {
-        }
+            private const string k_Title = "CountSpaces";
 
-        internal override void DoWhenSelected(object i_Object)
-        {
-            this.SelectedMenuItem += new SelectedEventHandler(buttonCountSpaces_SelectedMenuItem);
-            OnSelected(this);
-            this.SelectedMenuItem -= new SelectedEventHandler(buttonCountSpaces_SelectedMenuItem);
-        }
-
-        private void buttonCountSpaces_SelectedMenuItem(object i_Object)
-        {
-            string usersSentence = string.Empty;
-            uint numberOfSpaces = 0;
-
-            Console.WriteLine("Please enter a sentence");
-            usersSentence = Console.ReadLine();
-            foreach (char character in usersSentence)
+            public ButtonCountSpaces() : base(k_Title)
             {
-                if (character == ' ')
+            }
+
+            internal override void DoWhenSelected(object i_Object)
+            {
+                this.SelectedMenuItem += new SelectedEventHandler(buttonCountSpaces_SelectedMenuItem);
+                OnSelected(this);
+                this.SelectedMenuItem -= new SelectedEventHandler(buttonCountSpaces_SelectedMenuItem);
+            }
+
+            private void buttonCountSpaces_SelectedMenuItem(object i_Object)
+            {
+                string usersSentence = string.Empty;
+                uint numberOfSpaces = 0;
+
+                Console.WriteLine("Please enter a sentence");
+                usersSentence = Console.ReadLine();
+                foreach (char character in usersSentence)
                 {
-                    numberOfSpaces++;
+                    if (character == ' ')
+                    {
+                        numberOfSpaces++;
+                    }
                 }
-            }
 
-            if (numberOfSpaces == 1)
-            {
-                Console.WriteLine("There is {0} space in your sentence", numberOfSpaces);
-            }
-            else
-            {
-                Console.WriteLine("There are {0} spaces in your sentence", numberOfSpaces);
+                if (numberOfSpaces == 1)
+                {
+                    Console.WriteLine("There is {0} space in your sentence", numberOfSpaces);
+                }
+                else
+                {
+                    Console.WriteLine("There are {0} spaces in your sentence", numberOfSpaces);
+                }
             }
         }
     }
